cdccb5b3275aa683be748d594f0491be781e8a74
hammbino <hammondjeffrey@gmail.com>
2016-11-02 05:57:03 -0600

    Working Program. Needs to be cleaned up. Added did not cheat update.

--- src/JIMachine.java
+++ src/JIMachine.java
@@ -1,3 +1,14 @@
+//I declare that the following source code is my work.
+//I understand and can explain everything I have written, if asked.
+//I understand that copying any source code, in whole or in part,
+//that is not in my textbook nor provided or expressly permitted by the instructor,
+//constitutes cheating. I will receive a zero on this project for
+//poor academic performance if I am found in violation of this policy.
+
+//Jeffrey Hammond
+//CS3250
+//Project 4 - JIMachine
+
 import java.awt.*;
 import javax.swing.*;
 
